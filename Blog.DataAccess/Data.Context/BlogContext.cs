94cd5d46b9608f32cde68e7d2f5f76f0577bc52a
pawarmahen <pawarmahen.pawar@gmail.com>
2019-03-03 16:23:23 +0530

    blog context changes

--- Blog.DataAccess/Data.Context/BlogContext.cs
+++ Blog.DataAccess/Data.Context/BlogContext.cs
@@ -5,10 +5,10 @@ namespace Blog.DataAccess.Data.Context
 {
     internal class BlogContext : DbContext
     {
-        internal BlogContext() : base(@"Data Source=ASEN-L-ANANDK\SQLEXPRESSDEV;Initial Catalog=BlogApplication;User ID=sa;Password=Abcd$123")
-        {
-            Database.SetInitializer(new BlogInitializer());
-        }
+            internal BlogContext() : base(@"Data Source=ASEN-L-ANANDK\SQLEXPRESSDEV;Initial Catalog=BlogApplication;User ID=sa;Password=Abcd$123")
+            {
+                Database.SetInitializer(new BlogInitializer());
+            }
 
         internal DbSet<ArticlePage> ArticlePage { get; set; }
         internal DbSet<ArticleComments> Comments { get; set; }
