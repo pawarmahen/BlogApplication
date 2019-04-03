846641ff75b129d086da6250f2b66966b249518a
pawarmahen <pawarmahen.pawar@gmail.com>
2019-04-03 22:14:15 +0530

    article added

--- BlogApplication.Service/Controllers/ArticlePageController.cs
+++ BlogApplication.Service/Controllers/ArticlePageController.cs
@@ -2,6 +2,8 @@
 using Blog.DataAccess.Models;
 using Blog.DataAccess.Repository;
 using Unity;
+using System;
+using System.Collections.Generic;
 
 namespace BlogApplication.Service.Controllers
 {
@@ -15,9 +17,42 @@ namespace BlogApplication.Service.Controllers
         }
 
         [HttpGet]
+        [Route("article")]
         public ArticlePage Get(int id)
         {
-            return _repository.Get(id);
+            return new ArticlePage
+            {
+                Id = id,
+                Author = "Chacko Daniel",
+                Body =
+                    "<div class=\"Normal\">Azure Service Fabric is a distributed computing platform that makes it easy to build, deploy, and operate always-on, mission-critical microservice applications (containerized and non-containerized). Developers focus on building microservices, and the service fabric platform solves the hard problems like lifecycle management, orchestration, availability and scalability of the applications.</br>The platform also has a built -in,highly available,low - latency in memory state store with persistence.The developers can use this store via programmatic data structures(Dictionary and Queues) or as attached volume disk to build stateful microservices. </ br >Azure Service Fabric is open source and runs on both Windows and Linux,and you can use it either on - premises or in the cloud—it’s a great hybrid solution that can support demanding “cloud native” applications.</ br >Azure Service Fabric has three public offerings: Service Fabric Azure Cluster service, Service Fabric Standalone, and Azure Service Fabric Mesh service.</div>",
+
+                Comments = new List<ArticleComments>()
+                {
+                    new ArticleComments
+                    {
+                        Comment = "dsjfkdsjkbf"
+                    },
+                    new ArticleComments
+                    {
+                        Comment = "dsjfkdsjkbf"
+                    },
+                    new ArticleComments
+                    {
+                        Comment = "dsjfkdsjkbf"
+                    }
+                },
+                CreatedBy = "Chacko Daniel",
+                CreatedOn = DateTime.Now,
+                Description = "ghjhgjhgj",
+                Title = "Exploring Azure Service Fabric Mesh: A Platform for Building Mission Critical Microservices",
+                UpdatedBy = "Chacko Daniel",
+                UpdatedOn = DateTime.Now,
+                Image = new Uri(
+                    @"https://res.infoq.com/articles/azure-service-fabric-mesh/en/resources/1azure-service-fabric-mesh-1543001783194.jpg")
+
+            };
+            //return _repository.Get(id);
         }
     }
 }
